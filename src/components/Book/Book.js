24588441816e66dbafa4eca74d82712fb18198f9
Alberto Aragon <alberto@alturasoluciones.com>
2017-12-20 12:08:22 -0500

    feat: Rendering Book Independent Component
    Book Component created to render a book with all it's props.
    Also a variable for the initial books was created to render a
    subgroup of book based on the status of the book: reading, want, read.

---
+++ src/components/Book/Book.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import './Book.css'
+
+class Book extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    console.log(this.props.backgroundImageUrl)
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.backgroundImageUrl})` }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.bookTitle}</div>
+        <div className="book-authors">{this.props.bookAuthor}</div>
+      </div>
+    )
+  }
+}
+
+export default Book
